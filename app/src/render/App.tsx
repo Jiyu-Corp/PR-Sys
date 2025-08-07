a1a4135e5b34a183cc1448f7fdc92f3e1087d8ab
Romeu-Fuchs <romeu-ferreira@lojasmm.com.br>
2025-08-07 11:40:31 -0300

    Feat: Pagina-login-quase-completa

--- app/src/render/App.tsx
+++ app/src/render/App.tsx
@@ -1,21 +1,12 @@
 import logo from './assets/logo.png'
 import './App.css'
-import { MouseEvent } from 'react';
-
-const { access } = window.electron
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
 
-    async function handleForgotPassword(evt: MouseEvent) {
-        evt.stopPropagation();
-        console.log(await access.forgotPassword());
-    }
-
     return (
         <>
-            <h1>EPK</h1>
-            <h1>Test2</h1>
-            <button onClick={handleForgotPassword}>Test forgot password</button>
+            <LoginPage />
         </>
     )
 }
